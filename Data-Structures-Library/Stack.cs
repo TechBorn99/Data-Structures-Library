f8c144bc090398ec661cd1342105929bc131d218
Stefi99R <stefan.radojevic@uns.ac.rs>
2020-10-30 23:57:06 +0100

    Added structure for Stack

--- Data-Structures-Library/Stack.cs
+++ Data-Structures-Library/Stack.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Text;
 
 namespace Data_Structures_Library
 {
     /// <summary>
-    /// Class that models the Stack data structure, with FIFO (First In First Out) principle. This class contains methods for manipulating the stack.
+    /// Class that models the non-generic Stack data structure, with LIFO (Last In First Out) principle. This class contains methods for manipulating the stack.
     /// </summary>
     class Stack
     {
@@ -25,5 +26,74 @@ namespace Data_Structures_Library
                 this.next = next;
             }
         }
-    }
+
+        // Pointer to the last added element in the stack (top element)
+        private Node head;
+        // Number of elements in the stack
+        private int counter = 0;
+        // Elements in the stack, if the stack is implemented as an array
+        object[] elements;
+
+        /// <summary>
+        /// Constructor for Stack that is implemented through Singly Linked List.
+        /// </summary>
+        public Stack()
+        {
+            head = null;
+        }
+
+        public Stack(int MAXSIZE)
+        {
+            elements = new object[MAXSIZE];
+        }
+
+        /*/// <summary>
+        /// Method that adds an element at the top of the stack. Time complexity: O().
+        /// </summary>
+        /// <param name="value">Value that the new element will be initialized with.</param>
+        public void Push(object value)
+        {
+
+        }*/
+
+        /*
+            * 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public object Pop()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public object Peek()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool IsFull()
+        {
+
+        }
+
+        */
+    }  
 }
