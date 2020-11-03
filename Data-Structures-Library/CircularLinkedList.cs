769d64eaa3badabda06526a5883dc2abecb99055
Stefi99R <stefan.radojevic@uns.ac.rs>
2020-11-03 17:48:54 +0100

    Added basic structure for CircularLinkedList

---
+++ Data-Structures-Library/CircularLinkedList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Data_Structures_Library
+{
+    class CircularLinkedList
+    {
+        private class Node
+        {
+            internal object value;
+            internal Node next;
+
+            public Node(object value, Node next)
+            {
+                this.value = value;
+                this.next = next;
+            }
+        }
+
+        private Node head;
+        private Node tail;
+        private int counter = 0;
+
+    }
+}
