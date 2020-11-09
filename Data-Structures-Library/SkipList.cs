0e4ab9467f3db53f65b7971d565c7dbec9161b81
Stefi99R <stefan.radojevic@uns.ac.rs>
2020-11-09 23:56:56 +0100

    Added basic structure for Skip List

---
+++ Data-Structures-Library/SkipList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Data_Structures_Library
+{
+    public class SkipList
+    {
+        private class Node
+        {
+            internal static string pInf = "+oo";
+            internal static string nInf = "-oo";
+
+            internal Node next, prev, above, below;
+            internal string key;
+            internal object value;
+
+            public Node(string key, object value)
+            {
+                this.key = key;
+                this.value = value;
+
+                next = prev = above = below = null;
+            }
+
+            public Node(string key, object value, Node next, Node prev, Node above, Node below)
+            {
+                this.key = key;
+                this.value = value;
+                this.next = next;
+                this.prev = prev;
+                this.above = above;
+                this.below = below;
+            }
+        }
+
+        private Node head;
+        private Node tail;
+        private int lvl;
+        private int counter;
+
+    }
+}
