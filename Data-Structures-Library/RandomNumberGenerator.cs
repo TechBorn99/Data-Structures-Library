de619c14f0caeead8934f1576832bd5b6e085025
Stefi99R <stefan.radojevic@uns.ac.rs>
2020-11-10 15:15:38 +0100

    Added helper class RandomNumberGenerator

---
+++ Data-Structures-Library/RandomNumberGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures_Library
+{
+    class RandomNumberGenerator
+    {
+        private long m = 4294967296;
+        private const long a = 214013;
+        private const long c = 2531011;
+        private long min;
+        private long max;
+        private double seed;
+
+
+        public RandomNumberGenerator()
+        {
+            seed = DateTime.Now.Ticks % m;
+        }
+
+        public RandomNumberGenerator(long min, long max) : this()
+        {
+            this.min = min;
+            this.max = max;
+        }
+    }
+}
