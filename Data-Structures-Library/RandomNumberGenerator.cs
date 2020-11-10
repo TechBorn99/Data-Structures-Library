eba6543042657b66191e4fa0bd9ddd37d900ecab
Stefi99R <stefan.radojevic@uns.ac.rs>
2020-11-10 20:58:33 +0100

    Added XML comments for RandomNumberGenerator

--- Data-Structures-Library/RandomNumberGenerator.cs
+++ Data-Structures-Library/RandomNumberGenerator.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Data_Structures_Library
 {
+    /// <summary>
+    /// A helper class used for generating random numbers for probabilistic data structures, like SkipList. It contains only one method (Next), and two constructors.
+    /// </summary>
     class RandomNumberGenerator
     {
         private long m = 4294967296;
@@ -13,18 +16,29 @@ namespace Data_Structures_Library
         private long max;
         private double seed;
 
-
+        /// <summary>
+        /// A constructor for RandomNumberGenerator class objects, which only sets the seed.
+        /// </summary>
         public RandomNumberGenerator()
         {
             seed = DateTime.Now.Ticks % m;
         }
 
+        /// <summary>
+        /// A constructor for RandomNumberGenerator class objects, which can be used to generate random numbers that are inside the specified bounds.
+        /// </summary>
+        /// <param name="min">Lower limit for generating numbers.</param>
+        /// <param name="max">Upper limit for generating numbers.</param>
         public RandomNumberGenerator(long min, long max) : this()
         {
             this.min = min;
             this.max = max;
         }
 
+        /// <summary>
+        /// Method that generates random numbers, using a specific formula.
+        /// </summary>
+        /// <returns>Long (number).</returns>
         public long Next()
         {
             seed = (a * seed + c) % m;
