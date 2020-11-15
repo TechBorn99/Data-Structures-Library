4c22a0a4c5b8a240e1807ab7ecd1b0739a47364b
Stefi99R <stefan.radojevic@uns.ac.rs>
2020-11-15 23:57:46 +0100

    Added XML documentation and comments to the Heap

--- Data-Structures-Library/Heap.cs
+++ Data-Structures-Library/Heap.cs
@@ -2,14 +2,24 @@
 
 namespace Data_Structures_Library
 {
-    
+    /// <summary>
+    /// Implementation of Binary (Min/Max) Heap Data Structure.
+    /// </summary>
     class Heap
     {
+        // Number of elements in the Heap
         private int counter;
+        // Elements of the Heap
         private object[] elements;
+        // Boolean used for checking if the current Heap is Min or Max Heap
         private bool isMinHeap;
+        // Variable used for setting the capacity of the Heap, if it is not provided
         private const int CAPACITY = 32;
 
+        /// <summary>
+        /// Constructor for the Heap, using only one argument.
+        /// </summary>
+        /// <param name="isMin">Boolean determining whether Heap is Min or Max Heap.</param>
         public Heap(bool isMin = true)
         {
             elements = new object[CAPACITY];
@@ -17,6 +27,11 @@ namespace Data_Structures_Library
             counter = 0;
         }
 
+        /// <summary>
+        /// Constructor for the Heap, using two arguments.
+        /// </summary>
+        /// <param name="array">Array of object values, used to prefill the Heap.</param>
+        /// <param name="isMin">Boolean determining whether Heap is Min or Max Heap.</param>
         public Heap(object[] array, bool isMin = true)
         {
             elements = new object[array.Length];
