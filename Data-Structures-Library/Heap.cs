85343f41690fb2f201dd2b3a0ad15100645f0a63
Stefi99R <stefan.radojevic@uns.ac.rs>
2020-11-15 23:51:22 +0100

    Added Heap Data Structure

---
+++ Data-Structures-Library/Heap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Data_Structures_Library
+{
+    
+    class Heap
+    {
+        private int counter;
+        private object[] elements;
+        private bool isMinHeap;
+        private const int CAPACITY = 32;
+
+        public Heap(bool isMin = true)
+        {
+            elements = new object[CAPACITY];
+            isMinHeap = isMin;
+            counter = 0;
+        }
+
+        public Heap(object[] array, bool isMin = true)
+        {
+            elements = new object[array.Length];
+            Array.Copy(array, elements, array.Length);
+            isMinHeap = isMin;
+            counter = array.Length;
+
+            //BuildHeap();
+        }
+    }
+}
